6e05df14472fe7711ba9bcba87fe82a3649e9a02
elsdvlee <els.devleeschauwer@ugent.be>
2023-03-17 07:34:08 +0100

    support for R2RML added

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const prefixes = {
     fnml: 'http://semweb.mmlab.be/ns/fnml#',
     grel: 'http://users.ugent.be/~bjdmeest/function/grel.ttl#',
     ql: 'http://semweb.mmlab.be/ns/ql#',
+    d2rq: 'http://www.wiwiss.fu-berlin.de/suhl/bizer/D2RQ/0.1#'
 };
 
 let prefixesSPARQL = ''
@@ -69,11 +70,14 @@ async function workAroundIssue199(store) {
     }
 }
 
+
 const queryNoSelfJoins = `${prefixesSPARQL}  
-    SELECT * WHERE { 
+    SELECT *
+    WHERE { 
         ?TriplesMap rml:logicalSource ?LogicalSource.
         ?LogicalSource rml:source ?source .
         OPTIONAL { ?LogicalSource rml:iterator ?Iterator .}
+        OPTIONAL { ?LogicalSource rml:query ?Query .}
         ?TriplesMap rr:predicateObjectMap ?Pom.
         ?Pom rr:objectMap ?Om.
         ?Om rr:parentTriplesMap ?ParentTriplesMap.
@@ -83,41 +87,88 @@ const queryNoSelfJoins = `${prefixesSPARQL}
         ?ParentTriplesMap rml:logicalSource ?ParentLogicalSource.
         ?ParentLogicalSource rml:source ?source .
         OPTIONAL { ?ParentLogicalSource rml:iterator ?ParentIterator .}
+        OPTIONAL { ?ParentLogicalSource rml:query ?ParentQuery .}
         ?ParentTriplesMap rr:subjectMap ?ParentSubjectMap.
         ?ParentSubjectMap rr:template ?ParentSubjectTemplate.
 }`;
 
+// see https://stackoverflow.com/questions/64270284/sparql-find-subjects-with-the-same-set-of-triples
+const queryNoSelfJoinsComplexSource = `${prefixesSPARQL}
+    SELECT * WHERE{
+        ?TriplesMap rml:logicalSource ?LogicalSource.
+        OPTIONAL { ?LogicalSource rml:iterator ?Iterator .}
+        OPTIONAL { ?LogicalSource rml:query ?Query .}
+        ?TriplesMap rr:predicateObjectMap ?Pom.
+        ?Pom rr:objectMap ?Om.
+        ?Om rr:parentTriplesMap ?ParentTriplesMap.
+        OPTIONAL { ?Om rr:joinCondition ?JoinCondition.
+        ?JoinCondition rr:child ?Child.
+        ?JoinCondition rr:parent ?Parent.}
+        ?ParentTriplesMap rml:logicalSource ?ParentLogicalSource.
+        OPTIONAL { ?ParentLogicalSource rml:iterator ?ParentIterator .}
+        OPTIONAL { ?ParentLogicalSource rml:query ?ParentQuery .}
+        ?ParentTriplesMap rr:subjectMap ?ParentSubjectMap.
+        ?ParentSubjectMap rr:template ?ParentSubjectTemplate.  
+        ?LogicalSource rml:source ?s1 .
+        ?ParentLogicalSource rml:source ?s2 .
+        ?s1 ?p ?o .
+        ?s2 ?p ?o .
+        FILTER NOT EXISTS { ?s1 ?p1 ?o1 . FILTER NOT EXISTS { ?s2 ?p1 ?o1 } }
+        FILTER NOT EXISTS { ?s2 ?p2 ?o2 . FILTER NOT EXISTS { ?s1 ?p2 ?o2 } }
+}`
+const queryNoSelfJoinsComplexSourceR2RML = `${prefixesSPARQL}
+    SELECT * WHERE{
+        ?TriplesMap rr:logicalTable ?LogicalTable.
+        ?TriplesMap rr:predicateObjectMap ?Pom.
+        ?Pom rr:objectMap ?Om.
+        ?Om rr:parentTriplesMap ?ParentTriplesMap.
+        OPTIONAL { ?Om rr:joinCondition ?JoinCondition.
+        ?JoinCondition rr:child ?Child.
+        ?JoinCondition rr:parent ?Parent.}
+        ?ParentTriplesMap rr:logicalTable ?ParentLogicalTable.
+        ?ParentTriplesMap rr:subjectMap ?ParentSubjectMap.
+        ?ParentSubjectMap rr:template ?ParentSubjectTemplate.  
+        ?LogicalTable ?p ?o .
+        ?ParentLogicalTable ?p ?o .
+        FILTER NOT EXISTS { ?LogicalTable ?p1 ?o1 . FILTER NOT EXISTS { ?ParentLogicalTable ?p1 ?o1 } }
+        FILTER NOT EXISTS { ?ParentLogicalTable ?p2 ?o2. FILTER NOT EXISTS { ?LogicalTable ?p2 ?o2 } }
+}`
 
 async function eliminateSelfJoins(store) {
     await workAroundIssue199(store);
     // only for templates, conditions not take into account yet
-    const bindingsStream = await myEngine.queryBindings(queryNoSelfJoins, {sources: [store]});
-    const bindings = await bindingsStream.toArray();
-    for (const binding of bindings) {
+    const bindingsStream1 = await myEngine.queryBindings(queryNoSelfJoins, {sources: [store]});
+    const bindings1 = await bindingsStream1.toArray();
+    const bindingsStream2 = await myEngine.queryBindings(queryNoSelfJoinsComplexSource, {sources: [store]});
+    const bindings2 = await bindingsStream2.toArray();
+    const bindingsStream3= await myEngine.queryBindings(queryNoSelfJoinsComplexSourceR2RML, {sources: [store]});
+    const bindings3 = await bindingsStream3.toArray();
+    for (const binding of [...bindings1, ...bindings2, ...bindings3]) {
         //identify identical sources: 1. identical sources, part of the query  2. identical iterator (default is row, for csv and tsv)
-        let iterator = 'row'; //TODO add this to normalize-rml: default value for CSV and TSV, when done: adapt query
-        let parentIterator = 'row';
-        if (binding.has('Iterator')){iterator = binding.get('Iterator').value};
-        if (binding.has('ParentIterator')){parentIterator = binding.get('ParentIterator').value};
-        if (iterator === parentIterator) {
-            if (!binding.has('Child') || binding.get('Child').value === binding.get('Parent').value) {
+        let iterator = binding.has('Iterator') ? binding.get('Iterator').value : 'row'; //TODO add this to normalize-rml: default value for CSV and TSV, when done: adapt query
+        let parentIterator = binding.has('ParentIterator') ? binding.get('ParentIterator').value : 'row';
+        let query = binding.has('Query') ? binding.get('Query').value : 'none';
+        let parentQuery = binding.has('ParentQuery') ? binding.get('ParentQuery').value : 'none';
+        let child = binding.has('Child') ? binding.get('Child').value : 'none';
+        let parent = binding.has('Parent') ? binding.get('Parent').value : 'none';
+        if (iterator === parentIterator && query === parentQuery && child === parent ) {
+            //console.log('true');
+            store.removeQuads([
+                tripleNNN(binding.get('Om').value, prefixes.rr + 'parentTriplesMap', binding.get('ParentTriplesMap').value),
+                tripleNNN(binding.get('Om').value, prefixes.rdf + 'type', prefixes.rr + 'RefObjectMap')
+            ]);
+            if (binding.has('JoinCondition')) {
                 store.removeQuads([
-                    tripleNNN(binding.get('Om').value, prefixes.rr + 'parentTriplesMap', binding.get('ParentTriplesMap').value),
-                    tripleNNN(binding.get('Om').value, prefixes.rdf + 'type', prefixes.rr + 'RefObjectMap')
-                ]);
-                if (binding.has('JoinCondition')) {
-                    store.removeQuads([
-                        tripleNNN(binding.get('Om').value, prefixes.rr + 'joinCondition', binding.get('JoinCondition').value),
-                        tripleNNL(binding.get('JoinCondition').value, prefixes.rr + 'child', binding.get('Child').value),
-                        tripleNNL(binding.get('JoinCondition').value, prefixes.rr + 'parent', binding.get('Parent').value)
-                    ]);
-                }
-                store.addQuads([
-                    tripleNNN(binding.get('Om').value, prefixes.rdf + 'type', prefixes.rr + 'ObjectMap'),
-                    tripleNNL(binding.get('Om').value, prefixes.rr + 'template', binding.get('ParentSubjectTemplate').value),
-                    tripleNNN(binding.get('Om').value, prefixes.rr + 'termType', prefixes.rr + 'IRI')
+                    tripleNNN(binding.get('Om').value, prefixes.rr + 'joinCondition', binding.get('JoinCondition').value),
+                    tripleNNL(binding.get('JoinCondition').value, prefixes.rr + 'child', binding.get('Child').value),
+                    tripleNNL(binding.get('JoinCondition').value, prefixes.rr + 'parent', binding.get('Parent').value)
                 ]);
             }
+            store.addQuads([
+                tripleNNN(binding.get('Om').value, prefixes.rdf + 'type', prefixes.rr + 'ObjectMap'),
+                tripleNNL(binding.get('Om').value, prefixes.rr + 'template', binding.get('ParentSubjectTemplate').value),
+                tripleNNN(binding.get('Om').value, prefixes.rr + 'termType', prefixes.rr + 'IRI')
+            ]);
         }
     }
 }
@@ -136,7 +187,6 @@ async function makeLooseRML(store) {
     await eliminateSelfJoins(store);
     const bindingsStream = await myEngine.queryBindings(queryLooseRML, {sources: [store]});
     const bindings = await bindingsStream.toArray();
-
     for (const binding of bindings) {
         const parentSubjectTemplate = binding.get('ParentSubjectTemplate').value;
         const parentRef = "{" + binding.get('Parent').value + "}";
